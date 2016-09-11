c8e3362bd0b94906c96696ca4ba09f30957213f4
Steven Willett <s.willett@stevenwillett.com>
2016-09-11 12:55:19 +0100

    Added Explanations.js file
    Adding the defined explanations about Variables.

---
+++ explanations.js
@@ -0,0 +1,29 @@
+// Explanations
+
+// ### VARIABLES ### //
+var favColor = 'blue';
+var myFavColors = ['blue', 'red', 'green'];
+var numOfColors = 3;
+var hasGotFavColors = false;
+var richObject = {
+	firstName: 'Rich',
+	lastName: 'Armstrong',
+	favColors: ['blue', 'red', 'green'],
+	yearsAlive: 103,
+	isMale: true
+};
+
+richObject.firstName
+
+
+// ### FUNCTIONS ### //
+var area = function (width, height) {
+	return width * height;
+}
+var whatIsMyFavColor = function() {
+	return 'blue';
+}
+
+var doSomething = function() {
+	console.log("do something!");
+}
